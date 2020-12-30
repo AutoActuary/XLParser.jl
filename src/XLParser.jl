4c67ef0c8c3b73277df6dd0adfa00a963e89a366
simon <none@none.com>
2020-12-30 16:58:20 +0200

    Add Package and tests

--- src/XLParser.jl
+++ src/XLParser.jl
@@ -1,5 +1,161 @@
 module XLParser
+    export @xlexpr_str
+    export @xl_str
 
-# Write your package code here.
+    using Tokenize
+    using Espresso
+
+    # we bait and switch string concatenation (ampersand in xl) with the pipe
+    # character, since it placed on the appropriate precedence level in Julia:
+    # https://github.com/JuliaLang/julia/blob/master/src/julia-parser.scm
+    dummy_ampersand = :(|>)
+
+    function _sanitize_excel_formula_strings(str)
+        chars = collect(str)
+        str_builder = Array{Union{String, Char}, 1}()
+
+        i = 0
+        inquote = false
+        while (i+=1) <= length(chars)
+
+            # Mark the beginning of a string
+            if !inquote && chars[i] == '"'
+                inquote = true
+                push!(str_builder, "raw\"")
+
+            # escaping backslashes if they are before a quote (double them up)
+            elseif inquote && chars[i] == '\\'
+                cnt = 1
+                while (i+=1) <= length(chars) && chars[i] == '\\'
+                    cnt+=1
+                end
+
+                if chars[i] == '"'
+                    push!(str_builder, "\\"^(cnt*2))
+                else
+                    push!(str_builder, "\\"^(cnt))
+                end
+
+                # reset our peek-forward
+                i -= 1
+
+            # escaping of a quote
+            elseif inquote && (i+1) <= length(chars) && chars[i] == '"' && chars[i+1] == '"'
+                push!(str_builder, "\\\"")
+                i+=1
+
+            # closing the string
+            elseif inquote && chars[i] == '"'
+                push!(str_builder, '"')
+                inquote = false
+
+            # normal passthrough
+            else
+                push!(str_builder, chars[i])
+            end
+        end
+
+        return join(str_builder, "")
+    end
+
+
+    function _sanitize_excel_formula_tokens(str)
+        # ensure excel strings are converted to Julia syntax
+        str = _sanitize_excel_formula_strings(str)
+
+        tokens = collect(tokenize(str))
+
+        i = 0
+        while (i+=1) <= length(tokens)
+
+            # convert names to lowercase
+            if tokens[i].kind == Tokenize.Tokens.IDENTIFIER && tokens[i].val != lowercase(tokens[i].val)
+                tokens[i] = first(tokenize(lowercase(tokens[i].val)))
+                i-=1
+
+            # convert <> to !=
+            elseif (i != length(tokens) &&
+                tokens[i].kind == Tokenize.Tokens.LESS &&
+                tokens[i+1].kind == Tokenize.Tokens.GREATER)
+
+                tokens[i] = first(tokenize("!="))
+                tokens[i+1] = first(tokenize(""))
+
+            # convert = to ==
+            elseif (tokens[i].kind == Tokenize.Tokens.EQ)
+                tokens[i] = first(tokenize("=="))
+
+            # add whitespace between repeating minus signs to avoid syntax error
+            elseif (tokens[i].token_error == Tokenize.Tokens.INVALID_OPERATOR &&
+                        tokens[i].val == "--")
+                tokens[i] = first(tokenize("-"))
+                insert!(tokens, i+1, first(tokenize(" ")))
+                insert!(tokens, i+1, first(tokenize("-")))
+                insert!(tokens, i+1, first(tokenize(" ")))
+
+            # fill empty commas: FOO(,,) -> FOO(nothing,nothing,nothing)
+            elseif (tokens[i].kind == Tokenize.Tokens.COMMA)
+                # test if left of comma is bracket
+                j = i-1
+                if j >= 1 && tokens[j].kind == Tokenize.Tokens.WHITESPACE
+                    j -= 1
+                end
+
+                if j >= 1 && tokens[j].kind == Tokenize.Tokens.LPAREN
+                    insert!(tokens, j+1, first(tokenize("nothing")))
+                    i += 1
+                end
+
+                # test if right of comma is another comma or )
+                j = i+1
+                if j <= length(tokens) && tokens[j].kind == Tokenize.Tokens.WHITESPACE
+                    j += 1
+                end
+
+                if j <= length(tokens) && (tokens[j].kind == Tokenize.Tokens.COMMA ||
+                                           tokens[j].kind == Tokenize.Tokens.RPAREN)
+                    insert!(tokens, j, first(tokenize("nothing")))
+                    i += 1
+                end
+
+            # Replace ampersand with a lower precedence infix operator
+            elseif  tokens[i].kind == Tokenize.Tokens.AND
+                tokens[i] = first(tokenize(string(dummy_ampersand)))
+            end
+        end
+
+        return join(untokenize.(tokens), "")
+    end
+
+
+    function xlexpr(str)
+        parsable_string = _sanitize_excel_formula_tokens(str)
+        xl = Meta.parse(parsable_string)
+
+        # index syntax, eg: a[1:5]
+        xl = rewrite_all(xl, :(index(_a,_b):index(_a,_d)), :(_a[_b:_d]))
+        xl = rewrite_all(xl, :(index(_a,_b)), :(_a[_b]))
+
+        #TODO: test if index(_a,_b):index(_c,_d) exists and throw an error if so
+
+        # replace & infix with string function
+        xl = rewrite_all(xl, dummy_ampersand, :(string))
+        xl = rewrite_all(xl, :(string(string(_a...), _b)),
+                             :(string(_a..., _b)))
+
+        return xl
+    end
+
+
+    macro xlexpr_str(str)
+        # quot allows macro to not execute but to return the expression
+        Meta.quot(xlexpr(str))
+    end
+
+
+    macro xl_str(str)
+        # esc allows macro to execute in caller's scope
+        esc(xlexpr(str))
+    end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,55 @@
 using XLParser
 using Test
 
-@testset "XLParser.jl" begin
-    # Write your tests here.
+
+@testset "Sanitize Excel strings" begin
+
+    @test XLParser._sanitize_excel_formula_strings("\"hello\"") == "raw\"hello\""
+
+    @test(
+        XLParser._sanitize_excel_formula_strings(
+            "1 & \"hello\" & \"world\" & 2"
+        ) == "1 & raw\"hello\" & raw\"world\" & 2"
+    )
+
+    # use raw string examples without any escaping to improve readibility
+    for line in readlines((@__DIR__)*"/xl_formula_string_examples.txt")
+        strip(line) == "" && continue
+        i, j = strip.(split(line, "=>"))
+
+        @test XLParser._sanitize_excel_formula_strings(i) == j
+    end
+
+end
+
+
+@testset "Excel to Julia expression" begin
+
+    # dummy functions and variables to use in the examples
+    q(args...) = 1
+    vvv(args...) = 2
+    z = 1:20
+
+    @test xl"False = True" == false
+
+    @test xlexpr"INDEX(z,5):INDEX(z,10)" == :(z[5:10])
+
+    @test xl"INDEX(z,5):INDEX(z,10)" == [5,6,7,8,9,10]
+
+    @test xlexpr"a & b & c & d & e" == :(string(a, b, c, d, e))
+
+    # for some reason the expressions isn't equal although they are exactly the
+    # same and even have the same string representation:
+    @test(
+        string(xlexpr""" Q(1,2,,4) <> vvv(1e-8, "a""b""c" & "!" & INDEX(z,1)) & 2 """)==
+        string(:(q(1, 2, nothing, 4) != string(vvv(1.0e-8, string(raw"a\"b\"c", raw"!", z[1])), 2)))
+    )
+
+    @test xl"Q() & true" == "1true"
+
+    @test xl"Q() & true <> Q() & true" == false
+
+    @test xl"Q() & true = Q() & true" == true
+
+    @test xl""" "L" & 10*3+3 & "t" """ == "L33t"
 end
