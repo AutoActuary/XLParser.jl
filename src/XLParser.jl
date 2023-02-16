e210c44d6b48efdf7ac6fc158f49bc79ffcc6cd2
heetbeet <sfstreicher@gmail.com>
2023-02-16 15:47:40 +0200

    Added percentage to parser
    Also replaced Espresso for MacroTools

--- src/XLParser.jl
+++ src/XLParser.jl
@@ -1,194 +1,255 @@
 module XLParser
-    export @xlexpr_str
-    export @xl_str
-    export xlexpr
+export @xlexpr_str
+export @xl_str
+export xlexpr
 
-    using Tokenize
-    using Espresso
+using Tokenize
+using Espresso
+using MacroTools: MacroTools, @capture, postwalk
 
-    # we bait and switch string concatenation (ampersand in xl) with the pipe
-    # character, since it is placed on the appropriate precedence level in Julia:
-    # https://github.com/JuliaLang/julia/blob/master/src/julia-parser.scm
-    dummy_ampersand = :(|>)
+# we bait and switch string concatenation (ampersand in xl) with the pipe
+# character, since it is placed on the appropriate precedence level in Julia:
+# https://github.com/JuliaLang/julia/blob/master/src/julia-parser.scm
+dummy_ampersand = "|>"
 
-    function _sanitize_excel_formula_strings(str)
-        chars = collect(str)
-        str_builder = Array{Union{String, Char}, 1}()
+# same with percentage and ::ₓₓ℅ₓₓᵖᵉʳᶜᵉⁿᵗ
+dummy_percent = "::ːːˑ℅ˑᵖᵉʳᶜᵉⁿᵗˑːː"
 
-        i = 0
-        inquote = false
-        while (i+=1) <= length(chars)
+function _pretransform_excel_formula_string(str)
+    chars = collect(str)
+    str_builder = Array{Union{String,Char},1}()
 
-            # Mark the beginning of a string
-            if !inquote && chars[i] == '"'
-                inquote = true
-                push!(str_builder, "raw\"")
+    i = 0
+    inquote = false
+    while (i += 1) <= length(chars)
+
+        # Mark the beginning of a string
+        if !inquote && chars[i] == '"'
+            inquote = true
+            push!(str_builder, "raw\"")
 
             # escaping backslashes if they are before a quote (double them up)
-            elseif inquote && chars[i] == '\\'
-                cnt = 1
-                while (i+=1) <= length(chars) && chars[i] == '\\'
-                    cnt+=1
-                end
+        elseif inquote && chars[i] == '\\'
+            cnt = 1
+            while (i += 1) <= length(chars) && chars[i] == '\\'
+                cnt += 1
+            end
 
-                if chars[i] == '"'
-                    push!(str_builder, "\\"^(cnt*2))
-                else
-                    push!(str_builder, "\\"^(cnt))
-                end
+            if chars[i] == '"'
+                push!(str_builder, "\\"^(cnt * 2))
+            else
+                push!(str_builder, "\\"^(cnt))
+            end
 
-                # reset our peek-forward
-                i -= 1
+            # reset our peek-forward
+            i -= 1
 
             # escaping of a quote
-            elseif inquote && (i+1) <= length(chars) && chars[i] == '"' && chars[i+1] == '"'
-                push!(str_builder, "\\\"")
-                i+=1
+        elseif inquote && (i + 1) <= length(chars) && chars[i] == '"' && chars[i + 1] == '"'
+            push!(str_builder, "\\\"")
+            i += 1
 
             # closing the string
-            elseif inquote && chars[i] == '"'
-                push!(str_builder, '"')
-                inquote = false
+        elseif inquote && chars[i] == '"'
+            push!(str_builder, '"')
+            inquote = false
 
             # normal passthrough
-            else
-                push!(str_builder, chars[i])
-            end
+        else
+            push!(str_builder, chars[i])
         end
 
-        return join(str_builder, "")
+        # bait and switch ampersand and percentage operators
+        if !inquote && i <= length(chars)
+            if str_builder[end] == '%'
+                str_builder[end] = dummy_percent
+            elseif str_builder[end] == '&'
+                str_builder[end] = dummy_ampersand
+            end
+        end
     end
 
+    return join(str_builder, "")
+end
 
-    function _sanitize_excel_formula_tokens(str)
-        # ensure excel strings are converted to Julia syntax
-        str = _sanitize_excel_formula_strings(str)
-        str = replace(str, "\n" => " ")
+function _transform_excel_formula_string(str)
+    # ensure excel strings are converted to Julia syntax
+    str = _pretransform_excel_formula_string(str)
+    str = replace(str, "\n" => " ")
 
-        tokens = collect(tokenize(str))
+    tokens = collect(tokenize(str))
 
-        i = 0
-        while (i+=1) <= length(tokens)
+    i = 0
+    while (i += 1) <= length(tokens)
 
-            # convert names to lowercase
-            if tokens[i].kind == Tokenize.Tokens.IDENTIFIER && tokens[i].val != lowercase(tokens[i].val)
-                tokens[i] = first(tokenize(lowercase(tokens[i].val)))
-                i-=1
+        # convert names to lowercase
+        if tokens[i].kind == Tokenize.Tokens.IDENTIFIER &&
+            tokens[i].val != lowercase(tokens[i].val)
+            tokens[i] = first(tokenize(lowercase(tokens[i].val)))
+            i -= 1
 
             # convert if KEYWORD token to xl_if function token
-            elseif tokens[i].kind == Tokenize.Tokens.IF
-                tokens[i] = first(tokenize("xl_if"))
-
+        elseif tokens[i].kind == Tokenize.Tokens.IF
+            tokens[i] = first(tokenize("xl_if"))
 
             # convert <> to !=
-            elseif (i != length(tokens) &&
-                tokens[i].kind == Tokenize.Tokens.LESS &&
-                tokens[i+1].kind == Tokenize.Tokens.GREATER)
-
-                tokens[i] = first(tokenize("!="))
-                tokens[i+1] = first(tokenize(""))
+        elseif (
+            i != length(tokens) &&
+            tokens[i].kind == Tokenize.Tokens.LESS &&
+            tokens[i + 1].kind == Tokenize.Tokens.GREATER
+        )
+            tokens[i] = first(tokenize("!="))
+            tokens[i + 1] = first(tokenize(""))
 
             # convert = to ==
-            elseif (tokens[i].kind == Tokenize.Tokens.EQ)
-                tokens[i] = first(tokenize("=="))
+        elseif (tokens[i].kind == Tokenize.Tokens.EQ)
+            tokens[i] = first(tokenize("=="))
 
             # add whitespace between repeating minus signs to avoid syntax error
-            elseif (tokens[i].token_error == Tokenize.Tokens.INVALID_OPERATOR &&
-                        tokens[i].val == "--")
-                tokens[i] = first(tokenize("-"))
-                insert!(tokens, i+1, first(tokenize(" ")))
-                insert!(tokens, i+1, first(tokenize("-")))
-                insert!(tokens, i+1, first(tokenize(" ")))
+        elseif (
+            tokens[i].token_error == Tokenize.Tokens.INVALID_OPERATOR &&
+            tokens[i].val == "--"
+        )
+            tokens[i] = first(tokenize("-"))
+            insert!(tokens, i + 1, first(tokenize(" ")))
+            insert!(tokens, i + 1, first(tokenize("-")))
+            insert!(tokens, i + 1, first(tokenize(" ")))
 
             # fill empty commas: FOO(,,) -> FOO(nothing,nothing,nothing)
-            elseif (tokens[i].kind == Tokenize.Tokens.COMMA)
-                # test if left of comma is bracket
-                j = i-1
-                if j >= 1 && tokens[j].kind == Tokenize.Tokens.WHITESPACE
-                    j -= 1
-                end
+        elseif (tokens[i].kind == Tokenize.Tokens.COMMA)
+            # test if left of comma is bracket
+            j = i - 1
+            if j >= 1 && tokens[j].kind == Tokenize.Tokens.WHITESPACE
+                j -= 1
+            end
 
-                if j >= 1 && tokens[j].kind == Tokenize.Tokens.LPAREN
-                    insert!(tokens, j+1, first(tokenize("nothing")))
-                    i += 1
-                end
+            if j >= 1 && tokens[j].kind == Tokenize.Tokens.LPAREN
+                insert!(tokens, j + 1, first(tokenize("nothing")))
+                i += 1
+            end
 
-                # test if right of comma is another comma or )
-                j = i+1
-                if j <= length(tokens) && tokens[j].kind == Tokenize.Tokens.WHITESPACE
-                    j += 1
-                end
+            # test if right of comma is another comma or )
+            j = i + 1
+            if j <= length(tokens) && tokens[j].kind == Tokenize.Tokens.WHITESPACE
+                j += 1
+            end
 
-                if j <= length(tokens) && (tokens[j].kind == Tokenize.Tokens.COMMA ||
-                                           tokens[j].kind == Tokenize.Tokens.RPAREN)
-                    insert!(tokens, j, first(tokenize("nothing")))
-                    i += 1
-                end
-
-            # Replace ampersand with a lower precedence infix operator
-            elseif  tokens[i].kind == Tokenize.Tokens.AND
-                tokens[i] = first(tokenize(string(dummy_ampersand)))
+            if j <= length(tokens) && (
+                tokens[j].kind == Tokenize.Tokens.COMMA ||
+                tokens[j].kind == Tokenize.Tokens.RPAREN
+            )
+                insert!(tokens, j, first(tokenize("nothing")))
+                i += 1
             end
         end
-
-        return join(untokenize.(tokens), "")
     end
 
+    return join(untokenize.(tokens), "")
+end
 
-    function xlexpr(str)
-        # turn string into a valid (but logically wrong) Julia expression
-        parsable_string = _sanitize_excel_formula_tokens(str)
-        xl = Meta.parse(parsable_string)
+function xlexpr(str)
+    # turn string into a valid Julia expression (but still containing incorrect dummy operators wrong)
+    parsable_string = _transform_excel_formula_string(str)
+    xl = Meta.parse(parsable_string)
 
-        # now correct this logically wrong expression into a correct one
+    # now correct this logically wrong expression into a correct one
 
-        # index syntax, eg: a[1:5]
-        xl = rewrite_all(xl, :(index(_a,_b):index(_a,_d)), :(_a[_b:_d]))
-        xl = rewrite_all(xl, :(index(_a,_b)), :(_a[_b]))
-
-        #TODO: test expr contains index(_a,_b):index(_c,_d) and throw an error if so
-        xl = rewrite_all(xl, :(index(_a,_b):index(_a,_d)), :(_a[_b:_d]))
-
-        # replace & infix with string function
-        xl = rewrite_all(xl, dummy_ampersand, :(string))
-        xl = rewrite_all(xl, :(string(string(_a...), _b)),
-                             :(string(_a..., _b)))
-
-        # replace and(1,2,3) with 1 && (2 && 3)
-        while xl != (xl_new = rewrite_all(xl, :(and(_a, _b...)),
-                                              :(_a && and(_b...))))
-            xl = xl_new
+    # index syntax, eg: a[1:5]
+    postwalk(xl) do x
+        if @capture(x, index(a_, i₁_):index(c_, i₂_)) && a != c
+            throw(
+                ArgumentError(
+                    "indexing across different arrays `index(a, i):index(b, j)` is not supported",
+                ),
+            )
         end
-        xl = rewrite_all(xl, :(and(_a)), :(_a))
+        return x
+    end
 
+    xl = postwalk(xl) do x
+        @capture(x, index(var_, i₁_):index(var_, i₂_)) &&
+            return Expr(:ref, var, Expr(:call, :(:), i₁, i₂))
+        return x
+    end
 
-        # replace or(1,2,3) with 1 || (2 || 3)
-        while xl != (xl_new = rewrite_all(xl, :(or(_a, _b...)),
-                                              :(_a || or(_b...))))
-            xl = xl_new
+    xl = postwalk(xl) do x
+        @capture(x, index(var_, i_)) && return Expr(:ref, var, i)
+        return x
+    end
+
+    # replace |> (& replacement) infix with string function
+    xl = postwalk(xl) do x
+        # format off
+        if x isa Expr &&
+            x.head == :call &&
+            length(x.args) == 3 &&
+            strip(string(x.args[1])) == strip(dummy_ampersand)
+            return Expr(:call, :string, x.args[2], x.args[3])
         end
-        xl = rewrite_all(xl, :(or(_a)), :(_a))
-
-        # replace not(1) with !(1)
-        xl = rewrite_all(xl, :(not(_a)), :(!(_a)))
-
-        # replace excel if functions with julia if statements
-        xl = rewrite_all(xl, :(xl_if(_a,_b,_c)), :((_a ? _b : _c)))
-        xl = rewrite_all(xl, :(xl_if(_a,_b)), :((_a ? _b : false)))
-
-        return xl
+        return x
+        # format on
     end
 
-
-    macro xlexpr_str(str)
-        # quot allows macro to not execute but to return the expression
-        Meta.quot(xlexpr(str))
+    xl = postwalk(xl) do x
+        @capture(x, string(string(a__), b_)) && return Expr(:call, :string, a..., b)
+        return x
     end
 
-
-    macro xl_str(str)
-        # esc allows macro to execute in caller's scope
-        esc(xlexpr(str))
+    xl = MacroTools.prewalk(xl) do x
+        if @capture(x, and(a__))
+            length(a) == 0 && return x
+            length(a) == 1 && return a[1]
+            return Expr(:call, :(&&), a[1], Expr(:call, :and, a[2:end]...))
+        end
+        return x
     end
+
+    # replace or(1,2,3) with 1 || (2 || 3)
+    xl = MacroTools.prewalk(xl) do x
+        if @capture(x, or(a__))
+            length(a) == 0 && return x
+            length(a) == 1 && return a[1]
+            return Expr(:call, :(||), a[1], Expr(:call, :or, a[2:end]...))
+        end
+        return x
+    end
+
+    # replace not(1) with !(1)
+    xl = MacroTools.prewalk(xl) do x
+        if @capture(x, not(a_))
+            return Expr(:call, :!, a)
+        end
+        return x
+    end
+
+    xl = postwalk(xl) do x
+        if x isa Expr && x.head == :call && x.args[1] == :xl_if
+            return Expr(:if, [x.args[2:end]..., false, false, false][1:3]...)
+        end
+        return x
+    end
+
+    # Convert "::ːːˑ℅ˑᵖᵉʳᶜᵉⁿᵗˑːː" to "/100"
+    xl = postwalk(xl) do x
+        if x isa Expr &&
+            x.head == :(::) &&
+            string(x.args[2]) == replace(dummy_percent, ":" => "")
+            return Expr(:call, :/, x.args[1], 100)
+        end
+        return x
+    end
+
+    return xl
+end
+
+macro xlexpr_str(str)
+    # quot allows macro to not execute but to return the expression
+    return Meta.quot(xlexpr(str))
+end
+
+macro xl_str(str)
+    # esc allows macro to execute in caller's scope
+    return esc(xlexpr(str))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,25 +3,21 @@ using Test
 using Espresso
 
 @testset "Sanitize Excel strings" begin
-
-    @test XLParser._sanitize_excel_formula_strings("\"hello\"") == "raw\"hello\""
+    @test XLParser._pretransform_excel_formula_string("\"hello\"") == "raw\"hello\""
 
     @test(
-        XLParser._sanitize_excel_formula_strings(
-            "1 & \"hello\" & \"world\" & 2"
-        ) == "1 & raw\"hello\" & raw\"world\" & 2"
+        XLParser._pretransform_excel_formula_string("1 & \"hello\" & \"world\" & 2") ==
+            "1 |> raw\"hello\" |> raw\"world\" |> 2"
     )
 
     # use raw string examples without any escaping to improve readibility
-    for line in readlines((@__DIR__)*"/xl_formula_string_examples.txt")
+    for line in readlines((@__DIR__) * "/xl_formula_string_examples.txt")
         strip(line) == "" && continue
         i, j = strip.(split(line, "=>"))
 
-        @test XLParser._sanitize_excel_formula_strings(i) == j
+        @test XLParser._pretransform_excel_formula_string(i) == j
     end
-
 end
-
 
 @testset "Excel to Julia expression" begin
 
@@ -34,15 +30,27 @@ end
 
     @test xlexpr"INDEX(z,5):INDEX(z,10)" == :(z[5:10])
 
-    @test xl"INDEX(z,5):INDEX(z,10)" == [5,6,7,8,9,10]
+    @test_throws ArgumentError XLParser.xlexpr("INDEX(x,5):INDEX(z,10)")
+
+    @test xl"INDEX(z,5):INDEX(z,10)" == [5, 6, 7, 8, 9, 10]
+
+    @test xl"INDEX(z,5)" == 5
 
     @test xlexpr"a & b & c & d & e" == :(string(a, b, c, d, e))
+
+    # It displays correctly, but the expression is not exact equal
+    @test string(xlexpr"and(a, b, c, d, e)") == string(:(a && b && c && d && e))
 
     # for some reason the expressions isn't equal although they are exactly the
     # same and even have the same string representation:
     @test(
-        string(xlexpr""" Q(1,2,,4) <> vvv(1e-8, "a""b""c" & "!" & INDEX(z,1)) & 2 """)==
-        string(:(q(1, 2, nothing, 4) != string(vvv(1.0e-8, string(raw"a\"b\"c", raw"!", z[1])), 2)))
+        string(xlexpr""" Q(1,2,,4) <> vvv(1e-8, "a""b""c" & "!" & INDEX(z,1)) & 2 """) ==
+            string(
+            :(
+                q(1, 2, nothing, 4) !=
+                string(vvv(1.0e-8, string(raw"a\"b\"c", raw"!", z[1])), 2)
+            ),
+        )
     )
 
     @test xl"Q() & true" == "1true"
@@ -53,9 +61,27 @@ end
 
     @test xl""" "L" & 10*3+3 & "t" """ == "L33t"
 
+    @test string(Base.remove_linenums!((xlexpr"if(a, b, c)"))) ==
+        string(Base.remove_linenums!(:(
+        if a
+            b
+        else
+            c
+        end
+    )))
+
     # Equality in string representation but not in expression itself
-    a = xlexpr"if(or(and(1,2),5, 6),foo(5), 0)"
-    b = :(if 1 && 2 || (5 || 6) foo(5) else 0 end)
+    a = xlexpr"if(or(and(1,2,3),5, 6),foo(5), 0)"
+    b = :(
+        if 1 && (2 && 3) || (5 || 6)
+            foo(5)
+        else
+            0
+        end
+    )
     @test string(Base.remove_linenums!(a)) == string(Base.remove_linenums!(b))
 
+    @test xl"10%" == 0.1
+
+    @test xl"1+2*(3*4%*5+6)%" == 1 + 2 * (3 * (4 / 100) * 5 + 6) / 100
 end
